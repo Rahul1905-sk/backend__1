829f9312e3166ab7275980466ee8f330daf9019e
Rahul Singh <rahul1905.sk@gmail.com>
2023-05-02 11:13:49 +0530

    d

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const { userRoute } = require('./routes/User.routes')
 const { postRoute } = require('./routes/Post.routes')
 const { auth } = require('./middleware/auth.middleware')
 // const { auth } = require('./middleware/Auth.middleware')
+const cors = require('cors');
+
 
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 app.use("/users", userRoute)
 
